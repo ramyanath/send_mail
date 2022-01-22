844b19add263bca34e52f692dcaa82f54e6d8716
RAMYANATH MAJHI <72814530+ramyanath@users.noreply.github.com>
2022-01-23 01:07:45 +0530

    Update send.py

--- send.py
+++ send.py
@@ -4,7 +4,7 @@ import cgi
 # import mysql.connector
 import smtplib
 import pandas as pd
-# data=cgi.FieldStorage()
+
 # db=mysql.connector.connect(host="127.0.0.1",user="root",password="",db="test")
 # cursor=db.cursor()
 
@@ -18,13 +18,14 @@ e = pd.read_excel("Email.xlsx")
 result = e['Emails'].values
 SenderAddress = "ramyanathsgs@gmail.com"
 password = "password"
-
+data=cgi.FieldStorage()
+msg = data.getvalue('body')
+subject = data.getvalue('sub')
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 server.login(SenderAddress, password)
-msg = "Hello this mail is just for test purpose"
-subject = "test mail"
+
 body = "Subject: {}\n\n{}".format(subject,msg)
 for email in result:
     server.sendmail(SenderAddress, email, body)
